f73597629cc7037009bfd3df985db3a8387dfbc7
Yuichi Tanaka <yuichielectric@github.com>
2022-07-06 00:28:43 +0000

    Append .zshrc settings not to override the default .zshrc

--- install.sh
+++ install.sh
@@ -4,5 +4,18 @@ sh ./starship-install.sh -y
 mkdir -p $HOME/.config
 cp .config/starship.toml $HOME/.config/starship.toml
 
+# Install zsh-completions
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
+
+# Install zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
 # .zshrc
-cp .zshrc $HOME/
+echo "
+fpath+=${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions/src
+plugins=( 
+    # other plugins...
+    zsh-autosuggestions
+)
+eval "$(starship init zsh)"
+" >> $HOME/.zshrc
