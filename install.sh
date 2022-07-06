827c3d52d989a07c1689cbd226a5e0872a39ec50
Yuichi Tanaka <yuichielectric@github.com>
2022-07-06 00:59:12 +0000

    Install fzf

--- install.sh
+++ install.sh
@@ -10,5 +10,9 @@ git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.o
 # Install zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
+# Install fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+yes | ~/.fzf/install
+
 # .zshrc
 cp .zshrc $HOME/
